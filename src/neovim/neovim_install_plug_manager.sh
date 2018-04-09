1ed221986bf175a5a8e80d54460939471489fea6
Daniel Samson <daniel.samson@salesagility.com>
2018-04-10 00:06:58 +0100

    fix plug manager

--- src/neovim/neovim_install_plug_manager.sh
+++ src/neovim/neovim_install_plug_manager.sh
@@ -1,3 +1,3 @@
 function neovim_install_plug_manager {
-run_command_as_root "curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim";
+run_command "curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim";
 }
