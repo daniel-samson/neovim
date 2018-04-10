882e0e75d53023b558e56ab4341006ea259bb490
Daniel Samson <daniel.samson@salesagility.com>
2018-04-10 16:32:16 +0100

    fix install neovim on xenial

--- src/debian/debian_install_on_xenial.sh
+++ src/debian/debian_install_on_xenial.sh
@@ -2,7 +2,7 @@ function debian_install_on_xenial {
     echo "Preparing to install on Ubuntu Xenial";
     debian_apt_update;
     debian_install_python_support;
-    debian_install_neovim;
+    debian_install_neovim_xenial;
     debian_install_curl;
     debian_install_clipboard;
     debian_install_php_env;
