d36ee63fffddb98340e69d5262f73ee2497f315b
Daniel Samson <daniel.samson@salesagility.com>
2018-04-11 11:04:54 +0100

    updating fonts

--- install.sh
+++ install.sh
@@ -48,8 +48,8 @@ function debian_apt_update {
 run_command_as_root "apt update";
 }
 function debian_extract_hack_font {
-    sudo unzip Hack-v3.000-ttf.zip -d /usr/share/fonts
-    fc-cache
+    run_command_as_root "unzip Hack-v3.000-ttf.zip -d /usr/share/fonts"
+    run_command "fc-cache"
 }
 
 function debian_install_airline_fonts {
@@ -57,19 +57,19 @@ if [ -f /usr/share/fonts/ttf/Hack-Regular.ttf ]
 then
     echo "Found hack font"
 else
-    echo "Installing hack font"
-    HACK_FONT_URL="https://github.com/source-foundry/Hack/releases/download/v3.000/Hack-v3.000-ttf.zip"
+    echo "Installing hack font";
+    HACK_FONT_URL="https://github.com/source-foundry/Hack/releases/download/v3.000/Hack-v3.000-ttf.zip";
 
     if command -v curl
     then
-        curl -L -O ${HACK_FONT_URL}
-        extract_hack_font_debian
+        run_command "curl -L -O ${HACK_FONT_URL}";
+        extract_hack_font_debian;
     elif command -v wget
     then
-        wget ${HACK_FONT_URL}
-        extract_hack_font_debian
+        run_command "wget ${HACK_FONT_URL}";
+        extract_hack_font_debian;
     else
-        echo "cannot download hack font"
+        echo "cannot download hack font";
     fi
 fi
 }
