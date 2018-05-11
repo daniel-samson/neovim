f4d8374a7f6ca7c8eba525433c7887a605fc249b
Daniel Samson <daniel.samson@salesagility.com>
2018-05-11 13:44:21 +0100

    Add -y to add-apt-repository

--- src/debian/debian_install_neovim_xenial.sh
+++ src/debian/debian_install_neovim_xenial.sh
@@ -1,5 +1,5 @@
 function debian_install_neovim_xenial {
-run_command_as_root "add-apt-repository ppa:neovim-ppa/stable";
+run_command_as_root "add-apt-repository -y ppa:neovim-ppa/stable";
 run_command_as_root "apt-get update";
 run_command_as_root "apt-get install -y neovim";
 }
