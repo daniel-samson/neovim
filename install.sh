da8a9171f81e23f70b54fb38ae92509916bc9a91
Daniel Samson <daniel.samson@salesagility.com>
2018-05-18 08:12:38 +0100

    updating build process

--- install.sh
+++ install.sh
@@ -89,12 +89,12 @@ function debian_install_neovim {
 run_command_as_root "apt-get install -y neovim";
 }
 function debian_install_neovim_trusty {
-run_command_as_root "add-apt-repository ppa:neovim-ppa/stable";
+run_command_as_root "add-apt-repository -y ppa:neovim-ppa/stable";
 run_command_as_root "apt-get update";
 run_command_as_root "apt-get install -y neovim";
 }
 function debian_install_neovim_xenial {
-run_command_as_root "add-apt-repository ppa:neovim-ppa/stable";
+run_command_as_root "add-apt-repository -y ppa:neovim-ppa/stable";
 run_command_as_root "apt-get update";
 run_command_as_root "apt-get install -y neovim";
 }
