ed977eb787dda6d43754152a6e7815cca73f689e
Daniel Samson <daniel.samson@citnow.com>
2019-04-01 13:35:10 +0100

    Fix build - typo in curl installation

--- install.sh
+++ install.sh
@@ -90,15 +90,14 @@ run_command_as_root "pgkg -i bat-musl_0.10.0_amd64.deb";
 
 function debian_install_bat {
 echo "Installing bat";
-HACK_FONT_URL="https://github.com/sharkdp/bat/releases/download/v0.10.0/bat-musl_0.10.0_amd64.deb";
-
+BAT_URL="https://github.com/sharkdp/bat/releases/download/v0.10.0/bat-musl_0.10.0_amd64.deb";
 if command -v curl
 then
-    run_command "curl -L -O ${HACK_FONT_URL}";
+    run_command "curl -L -O ${BAT_URL}";
     debian_extract_hack_font;
 elif command -v wget
 then
-    run_command "wget ${HACK_FONT_URL}";
+    run_command "wget ${BAT_URL}";
     debian_extract_hack_font;
 else
     echo "cannot download bat";
